b4cb2114ef997c60da1a932f8ca20c753c348500
IlliaSatyrenko <denzp05@gmail.com>
2024-11-07 11:38:43 +0200

    finished

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿using System.Reflection.Metadata.Ecma335;
+﻿using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using System.Xml.Linq;
 using static SearchMethods.Common;
 using static SearchMethods.State;
 
@@ -89,15 +93,9 @@ namespace SearchMethods
         // Метод, що визначає, чи має стан розвз'язок
         public bool IsSolvable()
         {
-            int tileIndex = Array.IndexOf(numbers, (byte)0);
-            int row = GameTile.GetCoordinatesByIndex(tileIndex).yCord;
             int inv = CountInversions();
 
-            if (inv % 2 == 0 && row % 2 == 1)
-            {
-                return true;
-            }
-            else if (inv % 2 == 1 && row % 2 == 0)
+            if (inv % 2 == 0)
             {
                 return true;
             }
@@ -119,54 +117,83 @@ namespace SearchMethods
 
             return isSolved;
         }
-
-        //public static byte[] 
     }
-    
+
+    public class RBFSState: State
+    {
+        public EZeroDirection? previousState;
+        public int cost;
+        public int heuristic;
+        public int totalCost;
+
+        public RBFSState(byte[] nums, EZeroDirection? previousState, int cost): base (nums)
+        {
+            numbers = nums;
+            this.previousState = previousState;
+            this.cost = cost;
+            this.heuristic = GetHeuristic();
+            this.totalCost = heuristic + cost;
+        }
+    }
+
+    public class LDFSState : State
+    {
+        public EZeroDirection? previousState;
+
+        public LDFSState(byte[] nums, EZeroDirection? previousState) : base(nums)
+        {
+            numbers = nums;
+            this.previousState = previousState;
+        }
+    }
+
     public class LDFSMethod
     {
-        public List<EZeroDirection> GetCorrectPath(byte[] state, int limit)
+        public (List<EZeroDirection>, int, int, int, int) GetCorrectPath(byte[] state, int limit)
         {
             List<EZeroDirection> correctPath = new List<EZeroDirection>();
 
-            correctPath = LDFS(state, limit, correctPath, null).path;
+            int counter = 0;
+            int non = 0;
 
-            return correctPath;
+            correctPath = LDFS(new LDFSState(state, null), limit, correctPath, ref counter, ref non);
+
+            return (correctPath, counter, non, limit, counter);
         }
 
-        private (List<EZeroDirection> path, bool isCorrect) LDFS(byte[] state, int limit, List<EZeroDirection> path, EZeroDirection? direction)
+        private List<EZeroDirection> LDFS(LDFSState state, int limit, List<EZeroDirection> path, ref int counter, ref int non)
         {
-            bool isCorrect = false;
             List<EZeroDirection> tempPath = new List<EZeroDirection>(path);
-            if (direction != null)
+            if (state.previousState != null)
             {
-                tempPath.Add(direction.Value);
+                tempPath.Add(state.previousState.Value);
             }
 
-            if (IsSolved(state))
+            if (IsSolved(state.numbers))
             {
-                isCorrect = true;
-                return (tempPath, isCorrect);
+                return tempPath;
             }
             else if (limit == 0)
             {
-                return (new List<EZeroDirection>(), false);
+                non++;
+                return new List<EZeroDirection>();
             }
             else
             {
-                var movedStates = GetMovedStates(state, direction);
+                var movedStates = GetMovedStates(state.numbers, state.previousState);
 
                 foreach (var nextState in movedStates)
                 {
-                    (path, isCorrect) = LDFS(nextState.state, limit - 1, new List<EZeroDirection>(tempPath), nextState.direction);
-                    if (isCorrect)
+                    counter++;
+                    path = LDFS(new LDFSState(nextState.state, nextState.direction), limit - 1, new List<EZeroDirection>(tempPath), ref counter, ref non);
+                    if (path.Count != 0)
                     {
-                        return (path, isCorrect);
+                        return path;
                     }
                 }
             }
 
-            return (new List<EZeroDirection>(), false);
+            return new List<EZeroDirection>();
         }
 
         private List<(byte[] state, EZeroDirection direction)> GetMovedStates(byte[] currentState, EZeroDirection? direction)
@@ -215,61 +242,116 @@ namespace SearchMethods
 
     public class RBFSMethod
     {
-        public List<EZeroDirection> GetCorrectPath(byte[] state)
+        public (List<EZeroDirection>, int, int, int, int) GetCorrectPath(byte[] state)
         {
             List<EZeroDirection> correctPath = new List<EZeroDirection>();
 
             int counter = 0;
-            correctPath = RBFS(state, int.MaxValue, correctPath, null, 0, ref counter).path;
+            List<byte[]> max = new List<byte[]>();
+            List<byte[]> non = new List<byte[]>();
+            List<byte[]> allStates = new List<byte[]>();
 
-            return correctPath;
+            correctPath = RBFS(new RBFSState(state, null, 0), int.MaxValue, correctPath, ref counter, ref max, ref non, ref allStates).path;
+
+            return (correctPath, counter, non.Count, max.Count, allStates.Count);
         }
 
-        private (List<EZeroDirection> path, bool isCorrect, int newF) RBFS(byte[] state, int limit, List<EZeroDirection> path, EZeroDirection? direction, int cost, ref int counter)
+        private (List<EZeroDirection> path, int newF) RBFS(RBFSState state, int limit, List<EZeroDirection> path, ref int counter, ref List<byte[]> max, ref List<byte[]> non, ref List<byte[]> states)
         {
-            counter++;
-            bool isCorrect = false;
             List<EZeroDirection> tempPath = new List<EZeroDirection>(path);
-            if (direction != null)
+            if (state.previousState != null)
             {
-                tempPath.Add(direction.Value);
+                tempPath.Add(state.previousState.Value);
+            }
+            
+            if (!max.Contains(state.numbers))
+            {
+                max.Add(state.numbers);
             }
 
-            if (IsSolved(state))
+            if (non.Contains(state.numbers))
             {
-                return (tempPath, true, new State(state).GetHeuristic() + cost);
+                non.Remove(state.numbers);
             }
 
-            List<(byte[] state, int cost, EZeroDirection direction)> successors = new List<(byte[], int, EZeroDirection)>();
-
-            foreach (var nextState in GetMovedStates(state, direction))
+            if (!states.Contains(state.numbers))
             {
-                int f = Math.Max(new State(nextState.state).GetHeuristic() + (cost + 1), new State(state).GetHeuristic() + cost);
-                successors.Add((nextState.state, f, nextState.direction));
+                states.Add(state.numbers);
+            }
+
+            if (IsSolved(state.numbers))
+            {
+                return (tempPath, state.totalCost);
+            }
+
+            List<(RBFSState state, int val)> successors = new List<(RBFSState, int)>();
+
+            foreach (var nextState in GetMovedStates(state.numbers, state.previousState))
+            {
+                int f = Math.Max(new RBFSState(nextState.state, nextState.direction, state.cost + 1).totalCost, state.totalCost);
+                successors.Add((new RBFSState(nextState.state, nextState.direction, state.cost + 1), f));
+                
+                if (!states.Contains(nextState.state))
+                {
+                    states.Add(nextState.state);
+                }
+
+                if (!max.Contains(nextState.state))
+                {
+                    max.Add(nextState.state);
+                }
             }
 
             while (true)
             {
-                successors.Sort((x, y) => x.cost.CompareTo(y.cost));
-                var bestState = successors[0];
+                counter++;
+                successors.Sort((x, y) => x.val.CompareTo(y.val));
 
                 var alternative = limit;
                 if (successors.Count > 1)
                 {
-                     alternative = successors[1].cost;
+                     alternative = successors[1].val;
                 }
 
 
-                if (bestState.cost > limit)
+                if (successors[0].val > limit)
                 {
-                    return (new List<EZeroDirection>(), false, bestState.cost);
+                    if (!non.Contains(successors[0].state.numbers))
+                    {
+                        non.Add(successors[0].state.numbers);
+                    }
+
+                    var h = state.IsSolvable();
+
+                    if (max.Contains(successors[0].state.numbers))
+                    {
+                        max.Remove(successors[0].state.numbers);
+                    }
+
+                    if (successors.Count > 1 && max.Contains(successors[1].state.numbers))
+                    {
+                        max.Remove(successors[1].state.numbers);
+                    }
+
+                    if (successors.Count > 2 && max.Contains(successors[2].state.numbers))
+                    {
+                        max.Remove(successors[2].state.numbers);
+                    }
+
+                    return (new List<EZeroDirection>(), successors[0].val);
                 }
 
-                (path, isCorrect, bestState.cost) = RBFS(bestState.state, Math.Min(limit, alternative), new List<EZeroDirection>(tempPath), bestState.direction, cost + 1, ref counter);
+                var result = RBFS(successors[0].state, Math.Min(limit, alternative), new List<EZeroDirection>(tempPath), ref counter, ref max, ref non, ref states);
 
-                if (isCorrect)
+                path = result.path;
+
+                var tempSuccessor = successors[0];
+                tempSuccessor.val = result.newF;
+                successors[0] = tempSuccessor;
+
+                if (path.Count != 0)
                 {
-                    return (path, true, bestState.cost);
+                    return (path, successors[0].val);
                 }
 
             }
@@ -395,71 +477,150 @@ namespace SearchMethods
     {
         public static void Main()
         {
-            byte[] state = new byte[stateSize];
 
-            do
-            {
-                state = new byte[stateSize];
-
-                byte[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 0 };
-                for (int i = 0; i < stateSize; i++)
-                {
-                    int j = new Random().Next(nums.Length);
-                    state[i] = nums[j];
-
-                    nums = nums.Where(n => n != nums[j]).ToArray();
-                }
-            } while (!new State(state).IsSolvable());            
-
+            //state = new byte[]{ 
+            //    1,3,0,
+            //    4,2,6,
+            //    7,5,8 };
 
             var ldfs = new LDFSMethod();
             var rbfs = new RBFSMethod();
 
-            var rbfsPath = rbfs.GetCorrectPath(state);
-
-            Console.WriteLine("\n\nRBFS Path\n\n");
-
-            foreach (var dir in rbfsPath)
+            Console.WriteLine("\n\nRBFS\n\n");
+            for (int k = 0; k < 20; k++)
             {
-                switch (dir)
+                byte[] state = new byte[stateSize];
+
+                do
                 {
-                    case EZeroDirection.LeftDirection:
-                        Console.WriteLine("Right");
-                        break;
-                    case EZeroDirection.RightDirection:
-                        Console.WriteLine("Left");
-                        break;
-                    case EZeroDirection.UpDirection:
-                        Console.WriteLine("Down");
-                        break;
-                    case EZeroDirection.DownDirection:
-                        Console.WriteLine("Up");
-                        break;
+                    state = new byte[stateSize];
+
+                    byte[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 0 };
+                    for (int i = 0; i < stateSize; i++)
+                    {
+                        int j = new Random().Next(nums.Length);
+                        state[i] = nums[j];
+
+                        nums = nums.Where(n => n != nums[j]).ToArray();
+                    }
+                } while (!new RBFSState(state, null, 0).IsSolvable());
+
+                var rbfsPath = rbfs.GetCorrectPath(state);
+
+                for (int i = 0; i < 9; i += 3)
+                {
+                    for (int h = 0; h < 3; h++)
+                    {
+                        Console.Write(state[h + i] + " ");
+                    }
+                    Console.Write("\n");
+                }
+                Console.WriteLine("iterations: " + rbfsPath.Item2);
+                //Console.WriteLine("nodes in mem: " + rbfsPath.Item3);
+                Console.WriteLine("nodes in mem: " + rbfsPath.Item1.Count + 1);
+                Console.WriteLine("deadend: " + rbfsPath.Item4);
+                Console.WriteLine("nodes: " + rbfsPath.Item5);
+                Console.Write("\n");
+
+            }
+
+
+            Console.WriteLine("\n\nLDFS\n\n");
+            for (int k = 0; k < 20; k++)
+            {
+                byte[] state = new byte[stateSize];
+
+                do
+                {
+                    state = new byte[stateSize];
+
+                    byte[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 0 };
+                    for (int i = 0; i < stateSize; i++)
+                    {
+                        int j = new Random().Next(nums.Length);
+                        state[i] = nums[j];
+
+                        nums = nums.Where(n => n != nums[j]).ToArray();
+                    }
+                } while (!new LDFSState(state, null).IsSolvable());
+
+                var ldfsPath = ldfs.GetCorrectPath(state, 25);
+
+                var tmpState = state;
+
+                foreach (var dir in ldfsPath.Item1)
+                {
+                    tmpState = GameTile.Move(tmpState, dir).numbersState;
+                }
+
+                if (IsSolved(tmpState))
+                {
+                    for (int i = 0; i < 9; i += 3)
+                    {
+                        for (int h = 0; h < 3; h++)
+                        {
+                            Console.Write(state[h + i] + " ");
+                        }
+                        Console.Write("\n");
+                    }
+                    Console.WriteLine("iterations: " + ldfsPath.Item2);
+                    //Console.WriteLine("nodes in mem: " + ldfsPath.Item4);
+                    Console.WriteLine("nodes in mem: " + ldfsPath.Item1.Count + 1);
+                    Console.WriteLine("deadend: " + ldfsPath.Item3);
+                    Console.WriteLine("nodes: " + ldfsPath.Item5);
+                    Console.Write("\n");
+                }
+                else
+                {
+                    Console.WriteLine("No path found");
+                    Console.Write("\n");
                 }
             }
 
-            var ldfsPath = ldfs.GetCorrectPath(state, 12);
 
-            Console.WriteLine("\n\nLDFS Path\n\n");
+            //Console.WriteLine("\n\nRBFS Path\n\n");
 
-            foreach (var dir in ldfsPath)
-            {
-                switch (dir)
-                {
-                    case EZeroDirection.LeftDirection:
-                        Console.WriteLine("Right");
-                        break;
-                    case EZeroDirection.RightDirection:
-                        Console.WriteLine("Left");
-                        break;
-                    case EZeroDirection.UpDirection:
-                        Console.WriteLine("Down");
-                        break;
-                    case EZeroDirection.DownDirection:
-                        Console.WriteLine("Up");
-                        break;
-                }
-            }
+            //foreach (var dir in rbfsPath)
+            //{
+            //    switch (dir)
+            //    {
+            //        case EZeroDirection.LeftDirection:
+            //            Console.WriteLine("Right");
+            //            break;
+            //        case EZeroDirection.RightDirection:
+            //            Console.WriteLine("Left");
+            //            break;
+            //        case EZeroDirection.UpDirection:
+            //            Console.WriteLine("Down");
+            //            break;
+            //        case EZeroDirection.DownDirection:
+            //            Console.WriteLine("Up");
+            //            break;
+            //    }
+            //}
+
+            //var ldfsPath = ldfs.GetCorrectPath(state, 25);
+
+            //Console.WriteLine("\n\nLDFS Path\n\n");
+
+            //foreach (var dir in ldfsPath)
+            //{
+            //    switch (dir)
+            //    {
+            //        case EZeroDirection.LeftDirection:
+            //            Console.WriteLine("Right");
+            //            break;
+            //        case EZeroDirection.RightDirection:
+            //            Console.WriteLine("Left");
+            //            break;
+            //        case EZeroDirection.UpDirection:
+            //            Console.WriteLine("Down");
+            //            break;
+            //        case EZeroDirection.DownDirection:
+            //            Console.WriteLine("Up");
+            //            break;
+            //    }
+            //}
         }
     }
 }
